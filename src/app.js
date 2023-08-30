d057c1fe2f1d0c7343f783761d6845eea501a649
Vuong Hieu <vuonghieu@MacBook-Pro-cua-Vuong.local>
2023-08-30 12:34:12 +0900

    Refactor code: create routes and controllers folder

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const path = require('path');
+const app = express();
+
+const route = require('./routes');
+
+// app.use(morgan('combined'))
+app.use(express.static(path.join(__dirname,'public')));
+
+ //middleware
+ app.use(express.urlencoded({ extended: true }));
+ app.use(express.json());
+
+// Template engine
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname,'resources/views'));
+
+// Route init
+route(app);
+
+app.listen(3000);
