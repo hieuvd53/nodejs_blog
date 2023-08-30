9d7b6cb564e2d8c4d74850e1d418995c2d1b1f56
Vuong Hieu <vuonghieu@MacBook-Pro-cua-Vuong.local>
2023-08-30 18:51:46 +0900

    format file prettier

--- src/app.js
+++ src/app.js
@@ -17,7 +17,9 @@ app.use(express.json());
 app.engine('.hbs', engine({ extname: '.hbs' }));
         app.set("view engine", 
         '.hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', 
+
+      path.join(__dirname, 'resources/views'));
 
 // Route init
 route(app);
